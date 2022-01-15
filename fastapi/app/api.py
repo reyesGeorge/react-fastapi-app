f8f0d4de116dfa83967be2ca168f45f3ebd45304
reyesGeorge <georgereyes103@gmail.com>
2022-01-15 17:33:45 -0600

    react frontend to add meals and backend to post and get them as well as display

--- fastapi/app/api.py
+++ fastapi/app/api.py
@@ -2,6 +2,22 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
+meals = [
+    {
+        "Name": "Ground Turkey",
+        "Protein": 12,
+        "Carbs": 1,
+        "Fats": 2
+    },
+    {
+        "Name": "Ground Beef",
+        "Protein": 11,
+        "Carbs": 2,
+        "Fats": 4
+    },
+    
+]
+
 app = FastAPI()
 
 origins = [
@@ -22,3 +38,15 @@ app.add_middleware(
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Hey this is your sample."}
+
+
+# Our Get method for our meals
+@app.get("/meals", tags=["meals"])
+async def get_meals() -> dict:
+    return {"data": meals}
+
+# Our Post method for our meals
+@app.post("/meals", tags=["meals"])
+async def post_meals(meal: dict) -> dict:
+    meals.append(meal)
+    return {"data": {"We added a meal"}}
